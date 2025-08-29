18988d70986f8c2b3d551179d061cf1b347b0bb2
Ignacio Serrano <celestoskitocontact@gmail.com>
2025-08-29 11:05:29 -0600

    documentacion v1

--- app/src/main/java/org/example/cli/BuildCommand.java
+++ app/src/main/java/org/example/cli/BuildCommand.java
@@ -7,6 +7,25 @@ import javax.tools.JavaCompiler;
 import javax.tools.ToolProvider;
 import java.nio.file.Path;
 
+/**
+ * Proyecto: Expresso - Transpilador de lenguaje Expresso a Java
+ * Curso: [EIF400-II-2025] Paradigmas de Programacion
+ * Universidad Nacional de Costa Rica
+ * 
+ * Autores:
+ * - Kendall Miso Chinchilla Araya  -   119310542
+ * - Ignacio Serrano Gonzalez       -   402600631
+ * - Minor Brenes Aguilar           -   116730106
+ * - Pablo Chavarria Alvarez        -   117810573
+ * 
+ * Codigo de grupo: 02-1PM
+ * 
+ * Nota: Este codigo fue generado parcialmente con asistencia de IA
+ * y posteriormente modificado, adaptado y validado por el equipo
+ * de desarrollo para cumplir con los requerimientos especificos
+ * del proyecto.
+ */
+
 @Command(name = "build")
 public class BuildCommand implements Runnable {
 
--- app/src/main/java/org/example/cli/CommonOptions.java
+++ app/src/main/java/org/example/cli/CommonOptions.java
@@ -5,6 +5,25 @@ import java.nio.file.Paths;
 
 import picocli.CommandLine.Option;
 
+/**
+ * Proyecto: Expresso - Transpilador de lenguaje Expresso a Java
+ * Curso: [EIF400-II-2025] Paradigmas de Programacion
+ * Universidad Nacional de Costa Rica
+ * 
+ * Autores:
+ * - Kendall Miso Chinchilla Araya  -   119310542
+ * - Ignacio Serrano Gonzalez       -   402600631
+ * - Minor Brenes Aguilar           -   116730106
+ * - Pablo Chavarria Alvarez        -   117810573
+ * 
+ * Codigo de grupo: 02-1PM
+ * 
+ * Nota: Este codigo fue generado parcialmente con asistencia de IA
+ * y posteriormente modificado, adaptado y validado por el equipo
+ * de desarrollo para cumplir con los requerimientos especificos
+ * del proyecto.
+ */
+
 public class CommonOptions {
     @Option(names = {"--verbose"}) protected boolean verbose;
     @Option(names = {"--out"}) protected Path outputDir = Paths.get(".");
