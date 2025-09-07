4a13337418c7c1daa52c9871a67540ea3689bea3
Ignacio Serrano <celestoskitocontact@gmail.com>
2025-09-06 18:49:35 -0600

    junit fix1

--- app/src/test/java/una/paradigmas/AppTest.java
+++ app/src/test/java/una/paradigmas/AppTest.java
@@ -1,7 +1,7 @@
 package una.paradigmas;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Proyecto: Expresso - Transpilador de lenguaje Expresso a Java
